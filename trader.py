5a395e65ea075059879e4901aa979dffaf56acfa
Aigars <azael05x@gmail.com>
2017-08-22 08:40:21 +0300

    PyCharm reformatting code

--- trader.py
+++ trader.py
@@ -9,47 +9,49 @@ import backtrader as bt
 from rl_strategy import RLStrategy
 
 
-CURRENCY_PAIR = "BTC_ETH"
-TIMEFRAME = 300
-FILE_NAME = CURRENCY_PAIR + "_" + str(TIMEFRAME) + ".csv"
+def btc_eth_300():
+    currency_pair = "BTC_ETH"
+    timeframe = 300
+    file_name = currency_pair + "_" + str(timeframe) + "_small.csv"
+
+    modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
+    datapath = os.path.join(modpath, 'history/' + file_name)
+
+    return bt.feeds.GenericCSVData(
+        dataname=datapath,
+        name=currency_pair,
+        headers=True,
+        seperator=",",
+        # timeframe=bt.TimeFrame.Seconds,
+        # compression=timeframe,
+        dtformat="%Y/%m/%d %H:%M:%S",
+        fromdate=datetime(2017, 6, 1, 0, 0),
+        todate=datetime.now(),
+        volume=0,
+        high=1,
+        low=2,
+        datetime=3,
+        close=4,
+        open=5,
+        openinterest=-1
+    )
+
 
 if __name__ == '__main__':
+    # Init and configure cerebro
+    cerebro = bt.Cerebro()
+    cerebro.broker.setcash(100000)
+    # Add a FixedSize sizer according to the stake
+    cerebro.addsizer(bt.sizers.FixedSize, stake=1)
+    cerebro.broker.setcommission(commission=0.0)
+    # Add a strategy
+    cerebro.addstrategy(RLStrategy, n_bars=10)
 
-  # Init and configure cerebro
-  cerebro = bt.Cerebro()
-  cerebro.broker.setcash(10000)
-  # Add a FixedSize sizer according to the stake
-  cerebro.addsizer(bt.sizers.FixedSize, stake=1)
-  cerebro.broker.setcommission(commission=0.0)
-  # Add a strategy
-  cerebro.addstrategy(RLStrategy, bar_count=100)
+    # Load data
+    cerebro.adddata(btc_eth_300())
 
-  # Load data
-  modpath = os.path.dirname(os.path.abspath(sys.argv[0]))
-  datapath = os.path.join(modpath, 'history/' + FILE_NAME)
-  data = bt.feeds.GenericCSVData(
-    dataname=datapath,
-    name=CURRENCY_PAIR,
-    headers=True,
-    seperator=",",
-    # timeframe=bt.TimeFrame.Seconds,
-    # compression=TIMEFRAME,
-    dtformat="%Y/%m/%d %H:%M:%S",
-    fromdate=datetime(2017, 5, 1, 0, 0),
-    todate=datetime.now(),
-    volume=0,
-    high=1,
-    low=2,
-    datetime=3,
-    close=4,
-    open=5,
-    openinterest=-1
-  )
-  cerebro.adddata(data)
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    result = cerebro.run()
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
 
-
-  print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
-  result = cerebro.run()
-  print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
-
-  # cerebro.plot()
+    cerebro.plot()
